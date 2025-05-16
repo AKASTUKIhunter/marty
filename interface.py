281dcd80369b0c65a8c582701e4d46b363511a30
shadowTn <amen.allah.chaieb254@gmail.com>
2025-05-16 10:42:03 +0200

    Ajout des boutons de mouvement

--- interface.py
+++ interface.py
@@ -1,22 +1,42 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
 
 import sys
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.resize(800, 350)
+        self.setWindowTitle("Mr Marty")
+ 
+        button_avancer = QPushButton("Avancer", self)
+        button_avancer.setGeometry(900, 400, 100, 50)
+        button_avancer.move(110, 100)
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-        self.setFixedSize(QSize(400,300))
-        self.setCentralWidget(button)
+        button_reculer = QPushButton("V-droite", self)
+        button_reculer.setGeometry(900, 400, 100, 50)
+        button_reculer.move(210, 100)
+
+        button_reculer = QPushButton("V-Gauche", self)
+        button_reculer.setGeometry(900, 400, 100, 50)
+        button_reculer.move(10, 100)
         
 
+        button_reculer = QPushButton("Droite", self)
+        button_reculer.setGeometry(900, 400, 100, 50)
+        button_reculer.move(210, 150)
+
+        button_reculer = QPushButton("Reculer", self)
+        button_reculer.setGeometry(900, 400, 100, 50)
+        button_reculer.move(110, 200)
+
+        button_reculer = QPushButton("Gauche", self)
+        button_reculer.setGeometry(900, 400, 100, 50)
+        button_reculer.move(10, 150)
+
+        
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
-
 app.exec()
