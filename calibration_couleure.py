f2bc8fbde70c8cc8bf483d39e9c7f6fd16365d0c
christ-droid <christroimakanga08@gmail.com>
2025-05-16 11:02:26 +0200

    calibration couleure + IR

---
+++ calibration_couleure.py
@@ -0,0 +1,12 @@
+from martypy import Marty
+
+ip="192.168.0.104"
+my_marty = Marty("wifi",ip)
+
+color=my_marty.get_ground_sensor_reading('LeftColorSensor')
+print(color)
+
+IR=my_marty.get_ground_sensor_reading('RightIRFoot')
+print(IR)
+
+my_marty.close()
