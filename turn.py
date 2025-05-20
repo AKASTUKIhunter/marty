bf9e46a4ed78b8dc13d197aa7a1556fa4aea449d
sudo-yasminem <Yasmine_Moukhliss@etu.u-bourgogne.fr>
2025-05-20 16:26:13 +0200

    Turn

--- turn.py
+++ turn.py
@@ -5,13 +5,14 @@ martin = Marty("wifi","192.168.0.102")
 #Se met tout droit
 martin.get_ready()
 
-length_step = 20;
+length_step = 8;
 step_speed = 1500;
 angle = 20
 
-#Pour le nombre de pas i, marche arrière
-for i in range (0,10):
-    martin.walk(1,"left", angle, length_step,step_speed)
+#Pour le nombre de pas i,
+for i in range (0,5):
+    martin.walk(1,"left", -angle, length_step,step_speed)
     martin.walk(1,"right", -angle, length_step,step_speed)
 
+martin.close()
 
