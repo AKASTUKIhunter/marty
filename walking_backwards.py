39d4c9bb35112c488d105de9f194e2a27c2d1bbc
sudo-yasminem <Yasmine_Moukhliss@etu.u-bourgogne.fr>
2025-05-20 16:43:13 +0200

    Walking backwards OK

--- walking_backwards.py
+++ walking_backwards.py
@@ -5,16 +5,15 @@ martin = Marty("wifi","192.168.0.102")
 #Se met tout droit
 martin.get_ready()
 
-length_step = 1
+length_step = 15
 step_speed = 1000
 angle = 10
 
-#Pour le nombre de pas i,
+#Pour le nombre de pas i, marche arrière
 for i in range (0,5):
-    martin.stand_straight()
-    martin.walk(1,"left", angle, length_step,step_speed)
-    martin.stand_straight()
-    martin.walk(1,"right", -angle, length_step,step_speed)
+
+    martin.walk(1,"left",0, -length_step, step_speed)
+    martin.walk(1,"right", 0, -length_step, step_speed)
 
 
 martin.stand_straight()
