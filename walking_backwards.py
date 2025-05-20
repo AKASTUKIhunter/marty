0a2cb788a390630bd6154fb8dfe17ad3f3d05b18
sudo-yasminem <Yasmine_Moukhliss@etu.u-bourgogne.fr>
2025-05-20 16:50:16 +0200

    function Walking backwards OK

--- walking_backwards.py
+++ walking_backwards.py
@@ -1,21 +1,36 @@
 from martypy import Marty
 
-martin = Marty("wifi","192.168.0.102")
+
+
+marty = Marty("wifi","192.168.0.102")
+
+#Fonction moonwalk, prends en paramètre un nombre de pas à faire en moonwalk
+def walk_backwards(number_of_steps):
+    step_speed = 1000
+    length_step = 15
+    for i in range(0,number_of_steps):
+        marty.walk(1, "left", 0, -length_step, step_speed)
+        marty.walk(1, "right", 0, -length_step, step_speed)
+
+    marty.stand_straight()
+
+
 
 #Se met tout droit
-martin.get_ready()
+marty.get_ready()
 
+#Constantes pour la marche de Marty
 length_step = 15
 step_speed = 1000
 angle = 10
 
-#Pour le nombre de pas i, marche arrière
+#Pour le nombre de pas i, marche en arrière
 for i in range (0,5):
 
-    martin.walk(1,"left",0, -length_step, step_speed)
-    martin.walk(1,"right", 0, -length_step, step_speed)
+    marty.walk(1,"left",0, -length_step, step_speed)
+    marty.walk(1,"right", 0, -length_step, step_speed)
 
 
-martin.stand_straight()
+marty.stand_straight()
 
-martin.close()
+marty.close()
