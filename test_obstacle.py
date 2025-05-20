41d47dad273b15d1080483ed54e6627405e87f00
christ-droid <christroimakanga08@gmail.com>
2025-05-20 17:20:47 +0200

    test marche arrière et turn

---
+++ test_obstacle.py
@@ -0,0 +1,12 @@
+from martypy import Marty
+
+ip="192.168.0.102"
+my_marty = Marty("wifi",ip)
+
+while(1):
+    print(my_marty.foot_obstacle_sensed('left'))
+    if  my_marty.foot_obstacle_sensed('left'):
+        my_marty.dance()
+        
+    if  my_marty.foot_obstacle_sensed('right'):
+        my_marty.walk(3,'auto',0,10,1500)
