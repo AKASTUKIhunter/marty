879ac9596d17148a665a5e6a6c7d3284fec71ad8
sudo-yasminem <Yasmine_Moukhliss@etu.u-bourgogne.fr>
2025-05-23 09:50:08 +0200

    Kick update

--- kick.py
+++ kick.py
@@ -1,8 +1,14 @@
 from martypy import Marty
 
+#Kick Left
 def kickLeft(marty):
     marty.kick('left',0,2000,None)
 
-
+#Kick Right
 def kickRight(marty):
     marty.kick('right',0,2000,None)
+
+#Exemple de code
+marty = Marty("wifi","192.168.1.6")
+marty.get_ready()
+kickLeft(marty)
