f843cc0ab6e166415d11363feb582952bde0d6a4
sudo-yasminem <Yasmine_Moukhliss@etu.u-bourgogne.fr>
2025-06-06 14:52:44 +0200

    WalkCase and SideStepCase both directions function OK

--- moving_cells.py
+++ moving_cells.py
@@ -1,6 +1,8 @@
 from martypy import Marty
 from connect import MartyConnection
 
+
+
 marty_connec = MartyConnection("192.168.0.103")
 
 #Fonction pour traverser des cases en marchant
@@ -9,9 +11,14 @@ def WalkCase(marty_connec,nb_cases):
     for i in range(1,nb_cases):
         marty_connec.marty.walk(10, 'auto', 0,15,1500,None)
 
-#Fonction pour traverser des cases en sidestep
-def SideStepCase(marty_connec,nb_cases):
+#Fonction pour traverser des cases en sidestep gauche
+def SideStepCaseG(marty_connec,nb_cases):
     marty_connec.marty.get_ready()
     for i in range(1,nb_cases):
-        marty_connec.marty.sidestep('left', 5, 35, 1000) #Paramètres à ajouter après les mesures
+        marty_connec.marty.sidestep('left', 5, 35, 1000)
 
+#Fonction pour traverser des cases en sidestep droit
+def SideStepCaseD(marty_connec,nb_cases):
+    marty_connec.marty.get_ready()
+    for i in range(1,nb_cases):
+        marty_connec.marty.sidestep('right', 5, 35, 1000)
