b3535e3c60a32a70a967de85033ef2c74f876f04
sudo-yasminem <Yasmine_Moukhliss@etu.u-bourgogne.fr>
2025-06-12 10:34:25 +0200

    Writing feels updated

--- writing_feels.py
+++ writing_feels.py
@@ -1,16 +1,19 @@
 
 
-def CreateFeelsScript():
+def NewScript(cellcolor,mood,color_eyes):
     script = open("script.feels", 'wt')
-    return script
+    line = cellcolor + ";" + mood + ";" + color_eyes
+    script.write(line + "\n")
+    return script.name
 
 def Addline(script, cellcolor,mood,color_eyes):
-
-    line = cellcolor+";"+mood+";"+color_eyes
-    script.writelines(line)
+    script = open(script, "a")
+    line = cellcolor + ";" + mood + ";" + color_eyes
+    script.write(line + "\n")
+    script.close()
 
 
 #test
-fichier = CreateFeelsScript()
-Addline(fichier,"blue", "wide", "4080ff" "\n")
-Addline(fichier,"red", "wide", "4080ff" "\n")
+fichier = NewScript("blue","angry","ff0000")
+Addline(fichier,"blue", "wide", "4080ff")
+Addline(fichier,"red", "wide", "4080ff")
