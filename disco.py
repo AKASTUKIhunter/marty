698024e99833b929427682b727e15ec906b7c00b
sudo-yasminem <Yasmine_Moukhliss@etu.u-bourgogne.fr>
2025-06-12 10:08:05 +0200

    Disco color  updated

--- disco.py
+++ disco.py
@@ -3,5 +3,5 @@ from connect import MartyConnection
 marty_connec = MartyConnection()
 marty_connec.connect("192.168.1.6")
 
-marty_connec.marty.disco_named_pattern(marty_connec.marty.Disco.EYES, pattern="show-off")
+marty_connec.marty.disco_color('blue', marty_connec.marty.Disco.EYES, api='led')
 marty_connec.marty.dance()
